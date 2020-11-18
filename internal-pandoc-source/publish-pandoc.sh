2c7ecc400ab81190b49538863109c39ca2fea40a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-11-18 10:39:17 -0800

    publish all the binaries

--- internal-pandoc-source/publish-pandoc.sh
+++ internal-pandoc-source/publish-pandoc.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-echo "Copying 'pandoc' to '/usr/local/bin/'"
-cp /root/.cabal/bin/pandoc /usr/local/bin/
+echo "Copying binaries to '/usr/local/bin/'"
+cp /root/.cabal/bin/* /usr/local/bin/

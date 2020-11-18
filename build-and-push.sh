fa5deb3caeb9f65adb65161e4cf0289c7ca1725e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-11-18 11:17:33 -0800

    eliminate typing basename!

--- build-and-push.sh
+++ build-and-push.sh
@@ -2,5 +2,7 @@
 
 set -e
 
-sudo docker build --tag $DOCKER_REPO/$1 . > /tmp/$1-build.log 2>&1
-sudo docker push $DOCKER_REPO/$1 > /tmp/$1-push.log 2>&1 &
+export TAG=`basename $PWD`
+echo "Building $TAG"
+sudo docker build --tag $DOCKER_REPO/$TAG . > /tmp/$TAG.log 2>&1
+sudo docker push $DOCKER_REPO/$TAG >> /tmp/$TAG.log 2>&1 &

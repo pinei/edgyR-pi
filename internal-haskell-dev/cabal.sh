02028d86fbe314a6904f68c98e478e2326a258e1
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-11-20 14:45:14 -0800

    build cabal twice to get 3.2

--- internal-haskell-dev/cabal.sh
+++ internal-haskell-dev/cabal.sh
@@ -1,15 +1,19 @@
 #! /bin/bash
 
 export PATH=/root/.cabal/bin:$PATH
-cabal --version
-ghc --version
 cabal update
-
-echo "Building 'cabal-install'' with 'cabal'"
 /usr/bin/time cabal install \
   --jobs=12 \
   --disable-optimization \
-  --ghc-options="-fllvm" \
+  --overwrite-policy=always \
+cabal-install
+cabal user-config update
+
+cabal update
+/usr/bin/time cabal install \
+  --jobs=12 \
+  --disable-optimization \
+  --overwrite-policy=always \
 cabal-install
 cabal user-config update
 cabal --version

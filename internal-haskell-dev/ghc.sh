8614c0cd963a0b5b0603f78ac5c8074fb9434424
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-11-23 22:38:57 -0800

    default to full GHC

--- internal-haskell-dev/ghc.sh
+++ internal-haskell-dev/ghc.sh
@@ -4,11 +4,14 @@ cd $EDGYR_SRC
 wget -q -O - https://downloads.haskell.org/~ghc/$GHC_RELEASE/ghc-$GHC_RELEASE-src.tar.xz \
   | tar xJf -
 pushd ghc-$GHC_RELEASE
+if [ "$1" .eq "quick" ]
+then
   pushd mk
     cp build.mk.sample build.mk
     sed --in-place=.bak 's/\#BuildFlavour = quick$/BuildFlavour = quick/' build.mk
     diff build.mk.sample build.mk
   popd
+fi
 
   set -e
 

a370cd47c3f448beb85166081fbe479d984a2e94
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-11-25 09:55:13 -0800

    only upgrade to 3.0

--- internal-pandoc-source/cabal.sh
+++ internal-pandoc-source/cabal.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-export PATH=$EDGYR_BIN:$PATH
 which cabal
 cabal --version
 cabal user-config update
@@ -12,27 +11,12 @@ cabal update
   --disable-debug-info \
   --disable-documentation \
   --disable-executable-dynamic \
-  --disable-optimization \
   --disable-profiling \
 cabal-install
-
 $EDGYR_BIN/cabal user-config update
-$EDGYR_BIN/cabal new-update
-/usr/bin/time $EDGYR_BIN/cabal new-install \
-  --disable-coverage \
-  --disable-debug-info \
-  --disable-documentation \
-  --disable-executable-dynamic \
-  --disable-optimization \
-  --disable-profiling \
-  --ghc-options="-fasm" \
-  --overwrite-policy=always \
-cabal-install
 
 if [ ! -e /usr/local/bin/cabal ]
 then
   sudo cp --verbose --dereference $EDGYR_BIN/cabal /usr/local/bin/cabal
 fi
 ldd /usr/local/bin/cabal
-
-$EDGYR_BIN/cabal user-config update

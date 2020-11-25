83967b7a9cbb888f7e5b0bad32a0526ec905b051
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-11-25 11:42:54 -0800

    ready for smoke test

---
+++ edgyr-lab/configure-r-rstudio.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -e
+
+echo "Configuring R"
+/sbin/ldconfig --verbose
+R CMD javareconf
+
+echo "Configuring RStudio Server"
+useradd -r rstudio-server
+ln -f -s /usr/local/lib/rstudio-server/bin/rstudio-server /usr/sbin/rstudio-server
+mkdir -p /var/run/rstudio-server
+mkdir -p /var/lock/rstudio-server
+mkdir -p /var/log/rstudio-server
+mkdir -p /var/lib/rstudio-server
+mkdir -p /etc/rstudio

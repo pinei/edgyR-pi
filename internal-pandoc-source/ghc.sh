2896d13e1425e9b8a1e6bf878a98b708138c947b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-11-24 20:52:32 -0800

    two-job makes in GHC under 7 GB of RAM

--- internal-pandoc-source/ghc.sh
+++ internal-pandoc-source/ghc.sh
@@ -17,6 +17,23 @@ fi
 
   ./boot
   ./configure
-  /usr/bin/time make --jobs=`nproc`
+
+  # The Nano only has 4 GB of RAM. As a result, the GHC builds swamp the available
+  # RAM and multi-job makes are problematic - they swap and the system appears
+  # unresponsive.
+  #
+  # So we only run two-job makes if we have less than 7 GB of RAM.
+  export RAM_KILOBYTES=`grep MemTotal /proc/meminfo | sed 's/^MemTotal:  *//' | sed 's/ .*$//'`
+  if [ $RAM_KILOBYTES -ge "7000000" ]
+  then
+    export JOBS=`nproc`
+  else
+    export JOBS=2
+  fi
+  echo "Installing"
+  echo " -- starting GHC builds"
+  echo " -- RAM_KILOBYTES = $RAM_KILOBYTES; 'make' will use $JOBS jobs."
+
+  /usr/bin/time make --jobs=$JOBS
   sudo make install
 popd

9351a0ed57e2832867c4feb2ce01649c7f8c353c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-11-26 18:31:38 -0800

    cleanup of $BINARIES removal

--- edgyr-ml/configure-r-rstudio.sh
+++ edgyr-ml/configure-r-rstudio.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-echo "Installing binaries"
-cp -rp $BINARIES/bin /usr/local/
-cp -rp $BINARIES/lib /usr/local/
-cp -rp $BINARIES/include /usr/local/
-cp -rp $BINARIES/share /usr/local/
-
 echo "Configuring R"
 /sbin/ldconfig --verbose
 R CMD javareconf

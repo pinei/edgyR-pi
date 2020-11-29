5cb0bd8abce42ef04cee55d412e1fe474d0651b6
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-11-28 16:34:03 -0800

    push all the L4T images

---
+++ push-l4t.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+set -e
+
+sudo docker push edgyr/l4t-ml:r32.4.4-py3 > /tmp/push-l4t-ml_r32.4.4-py3.log 2>&1 &
+sudo docker push edgyr/l4t-ml:r32.4.4-tf1.15-py3 > /tmp/push-l4t-ml_r32.4.4-tf1.15-py3.log 2>&1 &
+sudo docker push edgyr/l4t-ml:r32.4.4-tf2.3-py3 > /tmp/push-l4t-ml_r32.4.4-tf2.3-py3.log 2>&1 &
+sudo docker push edgyr/l4t-tensorflow:r32.4.4-tf2.3-py3 > /tmp/push-l4t-tensorflow_r32.4.4-tf2.3-py3.log 2>&1 &
+sudo docker push edgyr/l4t-tensorflow:r32.4.4-tf1.15-py3 > /tmp/push-l4t-tensorflow_r32.4.4-tf1.15-py3.log 2>&1 &
+sudo docker push edgyr/l4t-pytorch:r32.4.4-pth1.6-py3 > /tmp/push-l4t-pytorch_r32.4.4-pth1.6-py3.log 2>&1 &
+wait

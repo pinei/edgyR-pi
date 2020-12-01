73c43084b01bc26f92bf0df651517c93bad7cdab
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-11-30 22:53:07 -0800

    don't use /tmp for logfiles

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -e
 
 export TAG=`basename $PWD`
 echo "Building $TAG"
-sudo docker build --tag $DOCKER_REPO/$TAG . > /tmp/$TAG.log 2>&1
+sudo docker build --tag $DOCKER_REPO/$TAG . > $HOME/build-logs/$TAG.log 2>&1
--- push-edgyr.sh
+++ push-edgyr.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-sudo docker push edgyr/edgyr:latest > /tmp/push-edgyr.log 2>&1 &
-sudo docker push edgyr/internal-rstudio-source:latest > /tmp/push-internal-rstudio-source.log 2>&1 &
-sudo docker push edgyr/internal-pandoc-source:latest > /tmp/push-internal-pandoc-source.log 2>&1 &
-sudo docker push edgyr/internal-r-source:latest > /tmp/push-internal-r-source.log 2>&1 &
+sudo docker push edgyr/edgyr:latest > $HOME/build-logs/push-edgyr.log 2>&1 &
+sudo docker push edgyr/internal-rstudio-source:latest > $HOME/build-logs/push-internal-rstudio-source.log 2>&1 &
+sudo docker push edgyr/internal-pandoc-source:latest > $HOME/build-logs/push-internal-pandoc-source.log 2>&1 &
+sudo docker push edgyr/internal-r-source:latest > $HOME/build-logs/push-internal-r-source.log 2>&1 &
 wait

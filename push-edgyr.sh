cfaf35dd2cf3519f64662ad691c3a96b211cfd63
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-12-04 00:39:14 -0800

    not using

--- push-edgyr.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-set -e
-
-sudo docker push edgyr/edgyr:latest > $HOME/build-logs/push-edgyr.log 2>&1 &
-sudo docker push edgyr/internal-rstudio-source:latest > $HOME/build-logs/push-internal-rstudio-source.log 2>&1 &
-sudo docker push edgyr/internal-pandoc-source:latest > $HOME/build-logs/push-internal-pandoc-source.log 2>&1 &
-sudo docker push edgyr/internal-r-source:latest > $HOME/build-logs/push-internal-r-source.log 2>&1 &
-wait

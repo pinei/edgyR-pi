0d0921ac8848147224494963716ca15d6aeb0acf
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-12-07 12:26:11 -0800

    only need to push two images!!

--- push-all.sh
+++ push-all.sh
@@ -4,10 +4,3 @@ set -e
 
 sudo docker push edgyr/edgyr:latest
 sudo docker push edgyr/internal-rstudio-server:latest
-sudo docker push edgyr/internal-r:latest
-sudo docker push edgyr/internal-pandoc:latest
-sudo docker push edgyr/internal-ghc-8.6:latest
-sudo docker push edgyr/internal-cabal-3.2:latest
-sudo docker push edgyr/internal-cabal-3.0:latest
-sudo docker push edgyr/internal-ghc-8.2:latest
-sudo docker push edgyr/internal-build-dependencies:latest

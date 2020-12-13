583c660bba329284234ceaa684027cc9ba1ac583
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-12-12 17:18:44 -0800

    don't log the pocl CUDA tests for now

--- internal-jetson-pocl/test-cuda.sh
+++ internal-jetson-pocl/test-cuda.sh
@@ -4,6 +4,6 @@ set -e
 
 pushd "pocl-$POCL_VERSION/build"
 echo "running CUDA tests"
-../tools/scripts/run_cuda_tests > $LOGS/run_cuda_tests.log 2>&1
+../tools/scripts/run_cuda_tests
 echo "CUDA tests finished"
 popd

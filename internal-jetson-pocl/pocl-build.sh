c935f2d381642adb29ca491c4f0878c79fbded4c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-12-13 22:47:18 -0800

    test for Xavier

--- internal-jetson-pocl/pocl-build.sh
+++ internal-jetson-pocl/pocl-build.sh
@@ -4,12 +4,23 @@ set -e
 
 pushd pocl
 mkdir --parents build; cd build
-cmake \
-  -G Ninja \
-  -DLLC_HOST_CPU=generic \
-  -DENABLE_CUDA=ON \
-  -DINSTALL_OPENCL_HEADERS=1 \
-..
+
+# LLVM 10 doesn't recognize a Xavier so we use "generic"
+if [ `grep -e "atomics fphp asimdhp" | wc -l` -gt "0" ]
+then
+  cmake \
+    -G Ninja \
+    -DLLC_HOST_CPU=generic \
+    -DENABLE_CUDA=ON \
+    -DINSTALL_OPENCL_HEADERS=1 \
+  ..
+else
+  cmake \
+    -G Ninja \
+    -DENABLE_CUDA=ON \
+    -DINSTALL_OPENCL_HEADERS=1 \
+  ..
+fi
 
 ninja
 ninja install

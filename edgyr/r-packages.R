cf90944cc532ca2e7619ecdfa8d8e3268729d951
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-12-15 14:32:47 -0800

    run V8 test inline

--- edgyr/r-packages.R
+++ edgyr/r-packages.R
@@ -15,6 +15,17 @@ install.packages(c(
   "V8"
 ), quiet = TRUE)
 
-source("~/Installers/R/test-V8.R")
-
 tinytex::install_tinytex()
+
+# test V8
+library(V8)
+
+# Create a new context
+print(ct <- v8())
+
+# Evaluate some code
+print(ct$eval("var foo = 123"))
+print(ct$eval("var bar = 456"))
+print(ct$eval("foo + bar"))
+cat(ct$eval("JSON.stringify({x:Math.random()})"))
+print(ct$eval("(function(x){return x+1;})(123)"))

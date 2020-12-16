3209e0e1a506d33ea59ab3b91622c0a7be3ba608
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-12-15 17:21:11 -0800

    fix test

--- edgyr/r-packages.R
+++ edgyr/r-packages.R
@@ -15,17 +15,16 @@ install.packages(c(
   "V8"
 ), quiet = TRUE)
 
-tinytex::install_tinytex()
-
 # test V8
 library(V8)
 
 # Create a new context
-print(ct <- v8())
+ct <- v8()
 
 # Evaluate some code
-print(ct$eval("var foo = 123"))
-print(ct$eval("var bar = 456"))
-print(ct$eval("foo + bar"))
-cat(ct$eval("JSON.stringify({x:Math.random()})"))
-print(ct$eval("(function(x){return x+1;})(123)"))
+ct$eval("var foo = 123")
+ct$eval("var bar = 456")
+if (ct$eval("foo + bar") != 579) { stop(paste0(ct$eval("foo + bar"), " wrong value")) }
+
+# install TinyTeX
+tinytex::install_tinytex()

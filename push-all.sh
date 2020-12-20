4b3491a7438aa62736fc38d30103b849eeb3e545
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-12-19 21:18:38 -0800

    push the internal images first

--- push-all.sh
+++ push-all.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-sudo docker push edgyr/edgyr:latest
-sudo docker push edgyr/internal-libnode-dev:latest
 sudo docker push edgyr/internal-jetson-pocl:latest
+sudo docker push edgyr/internal-libnode-dev:latest
+sudo docker push edgyr/edgyr:latest

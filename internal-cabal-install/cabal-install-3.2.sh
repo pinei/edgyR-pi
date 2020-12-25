ef1850750fb75578e1e921da7586ba34dff41a6d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-12-25 13:24:09 -0800

    move all timing to Dockerfile

--- internal-cabal-install/cabal-install-3.2.sh
+++ internal-cabal-install/cabal-install-3.2.sh
@@ -14,7 +14,7 @@ which cabal
 cabal --version
 cabal user-config update
 cabal update
-/usr/bin/time cabal install \
+cabal install \
   --disable-benchmarks \
   --disable-coverage \
   --disable-debug-info \

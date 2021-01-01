0f5c8a86265fbdb2aec8e6a3fcbd2259d79bb2c6
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-01-01 15:13:07 -0800

    tag image with :backup before rebuild

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@
 set -e
 
 export TAG=`basename $PWD`
+echo "Creating a backup"
+sudo docker tag $DOCKER_REPO/$TAG:latest $DOCKER_REPO/$TAG:backup
 echo "Building $TAG"
 sudo docker build --tag $DOCKER_REPO/$TAG .

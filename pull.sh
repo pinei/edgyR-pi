9a891e6238a3c3a5fcae078be6562e43b37ff665
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-01-01 17:06:11 -0800

    add PyTorch 1.7 to pull script

--- pull.sh
+++ pull.sh
@@ -4,4 +4,5 @@ set -e
 
 sudo docker pull edgyr/internal-jetson-pocl:latest
 sudo docker pull edgyr/internal-libnode-dev:latest
+sudo docker pull edgyr/l4t-pytorch:r32.4.4-pth1.7-py3
 sudo docker pull edgyr/edgyr:latest

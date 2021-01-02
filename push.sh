9e6b3f4447d6407f75928ad3dda890243373af56
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-01-01 16:54:48 -0800

    push PyTorch 1.7 image too

--- push.sh
+++ push.sh
@@ -4,4 +4,5 @@ set -e
 
 sudo docker push edgyr/internal-jetson-pocl:latest
 sudo docker push edgyr/internal-libnode-dev:latest
+sudo docker push edgyr/l4t-pytorch:r32.4.4-pth1.7-py3
 sudo docker push edgyr/edgyr:latest

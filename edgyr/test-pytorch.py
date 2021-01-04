56a7e7683d624c76352074ff32079441ee471e36
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-01-03 17:14:11 -0800

    Install PyTorch 1.7 directly

---
+++ edgyr/test-pytorch.py
@@ -0,0 +1,16 @@
+import torch
+print('torch version: ' + torch.__version__)
+print('CUDA available: ' + str(torch.cuda.is_available()))
+print('cuDNN version: ' + str(torch.backends.cudnn.version()))
+a = torch.cuda.FloatTensor(2).zero_()
+print('Tensor a = ' + str(a))
+b = torch.randn(2).cuda()
+print('Tensor b = ' + str(b))
+c = a + b
+print('Tensor c = ' + str(c))
+
+import torchvision
+print('torchvision version: ' + torchvision.__version__)
+
+import torchaudio
+print('torchaudio version: ' + torchaudio.__version__)

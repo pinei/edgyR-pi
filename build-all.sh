05cda79efcf8fd145092929909ead393dc1864ae
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-01-04 00:07:54 -0800

    rewewritten for new image names

--- build-all.sh
+++ build-all.sh
@@ -4,23 +4,9 @@ set -e
 
 grep -i -e "BogoMIPS" /proc/cpuinfo
 grep -i -e "MemTotal" /proc/meminfo
-for i in \
-  internal-cabal-install \
-  internal-pandoc \
-  internal-r \
-  internal-rstudio-server \
-  internal-libnode-dev
-do
-  pushd $i
-  ../build.sh
-  popd
-done
-
-# run the rest only on Jetson
-if [ `uname -m` != "x86_64" ]
-then 
-  pushd internal-jetson-pocl; ../build.sh; popd
-  pushd edgyr; ../build.sh; popd
-fi
+pushd internal-ubuntu-builder; ../build.sh &; popd
+pushd internal-jetson-pocl; ../build.sh &; popd
+wait
+pushd edgyr; ../build.sh; popd
 
 docker images

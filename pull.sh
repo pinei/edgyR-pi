1954ee464702af1728876c3cc4fb6021fa9fcfa1
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-01-08 14:22:58 -0800

    pull "latest"

--- pull.sh
+++ pull.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-export EDGYR_RELEASE=0.7.4.9999
 for image in \
   internal-pandoc \
   internal-ubuntu-builder \
   edgyr
 do
-  sudo docker pull "edgyr/$image:$EDGYR_RELEASE"
+  sudo docker pull "edgyr/$image:latest"
 done

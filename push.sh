63f700477c70b48e36c0f0a3858d1cc5261f3fb7
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-01-08 14:21:43 -0800

    always push "latest"!!

--- push.sh
+++ push.sh
@@ -8,6 +8,10 @@ for image in \
   internal-ubuntu-builder \
   edgyr
 do
-  sudo docker tag "edgyr/$image:latest" "edgyr/$image:$EDGYR_RELEASE"
-  sudo docker push "edgyr/$image:$EDGYR_RELEASE"
+  sudo docker push "edgyr/$image:latest"
+  if [ ${#EDGYR_RELEASE} -gt "0" ]
+  then 
+    sudo docker tag "edgyr/$image:latest" "edgyr/$image:$EDGYR_RELEASE"
+    sudo docker push "edgyr/$image:$EDGYR_RELEASE"
+  fi
 done

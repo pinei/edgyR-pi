a53d3223beb40e21af04f16fc40ea6adb8aa536f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-01-16 21:41:03 -0800

    Installer for arrow-cpp 3.0.0 / pyarrow / R 'arrow'!!

---
+++ build-scripts/edgyr/Installers/pyarrow-cuda.sh
@@ -0,0 +1,66 @@
+#! /bin/bash
+
+set -e
+
+sudo apt-get update
+sudo apt-get upgrade -y
+export ARROW_VERSION=3.0.0
+cd $PROJECT_HOME
+rm -fr arrow*
+echo "Fetching source tarball 'apache-arrow-$ARROW_VERSION.tar.gz'"
+curl -Ls https://github.com/apache/arrow/archive/apache-arrow-$ARROW_VERSION.tar.gz \
+  | tar xzf -
+mv arrow-apache-arrow-$ARROW_VERSION arrow
+
+echo "Patching arrow-cpp"
+diff $HOME/Installers/etc/CMakeLists.txt-cuda-patch arrow/cpp/src/arrow/gpu/CMakeLists.txt || true
+cp $HOME/Installers/etc/CMakeLists.txt-cuda-patch arrow/cpp/src/arrow/gpu/CMakeLists.txt
+diff $HOME/Installers/etc/CMakeLists.txt-cuda-patch arrow/cpp/src/arrow/gpu/CMakeLists.txt
+
+echo "Creating conda env 'pyarrow-cuda'"
+source $HOME/miniconda3/etc/profile.d/conda.sh
+conda create --quiet --force --yes --name pyarrow-cuda --channel conda-forge \
+    --file arrow/ci/conda_env_unix.yml \
+    --file arrow/ci/conda_env_cpp.yml \
+    --file arrow/ci/conda_env_python.yml \
+    --file arrow/ci/conda_env_gandiva.yml \
+    python=3.6 \
+    pandas >> $EDGYR_LOGS/pyarrow.log 2>&1
+conda activate pyarrow-cuda
+export ARROW_HOME=$CONDA_PREFIX
+
+echo "Configuring arrow-cpp"
+mkdir --parents arrow/cpp/build
+pushd arrow/cpp/build
+cmake -DCMAKE_INSTALL_PREFIX=$ARROW_HOME \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DARROW_WITH_BZ2=ON \
+      -DARROW_WITH_ZLIB=ON \
+      -DARROW_WITH_ZSTD=ON \
+      -DARROW_WITH_LZ4=ON \
+      -DARROW_WITH_SNAPPY=ON \
+      -DARROW_WITH_BROTLI=ON \
+      -DARROW_CUDA=ON \
+      -DARROW_PARQUET=ON \
+      -DARROW_PYTHON=ON \
+      -GNinja \
+      .. >> $EDGYR_LOGS/pyarrow.log 2>&1
+
+echo "Installing arrow-cpp"
+ninja -j `nproc` >> $EDGYR_LOGS/pyarrow.log 2>&1
+ninja install >> $EDGYR_LOGS/pyarrow.log 2>&1
+popd
+
+echo "Installing pyarrow"
+pushd arrow/python
+export PYARROW_WITH_CUDA=1
+export PYARROW_WITH_PARQUET=1
+python setup.py build_ext --inplace >> $EDGYR_LOGS/pyarrow.log 2>&1
+popd
+
+echo "Installing R package 'arrow'"
+export PKG_CONFIG_PATH=$CONDA_PREFIX/lib/pkgconfig
+export R_LD_LIBRARY_PATH=$R_LD_LIBRARY_PATH:$CONDA_PREFIX/lib
+pushd arrow/r
+R CMD INSTALL . >> $EDGYR_LOGS/pyarrow.log 2>&1
+popd

b7bdd9864e425b90196affca1b3a91538a6595db
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-02-16 19:35:14 +0000

    add faust

--- build-scripts/edgyr/Installers/synths.sh
+++ build-scripts/edgyr/Installers/synths.sh
@@ -2,15 +2,17 @@
 
 set -e
 
-export CSOUND_VERSION=6.15.0
 export CHUCK_VERSION=1.4.0.1
+export CSOUND_VERSION=6.15.0
+export FAUST_VERSION=2.30.5
 echo "Installing build dependencies"
 sudo apt-get install -qqy --no-install-recommends \
   bison \
   flex \
   gettext \
+  libmicrohttpd-dev \
   swig3.0 \
-  >> $EDGYR_LOGS/audio.log 2>&1
+  >> $EDGYR_LOGS/synths.log 2>&1
 
 mkdir --parents $HOME/src
 cd $HOME/src
@@ -28,14 +30,14 @@ cmake \
   -DBUILD_STATIC_LIBRARY=ON \
   -DLAME_HEADER="/usr/include/lame/lame.h" \
   -DPULSEAUDIO_HEADER="/usr/include/pulse/simple.h" \
-  ..  >> $EDGYR_LOGS/audio.log 2>&1
+  ..  >> $EDGYR_LOGS/synths.log 2>&1
 echo "Compiling CSound"
 /usr/bin/time make --jobs=`nproc` \
-  >> $EDGYR_LOGS/audio.log 2>&1
+  >> $EDGYR_LOGS/synths.log 2>&1
 echo "Installing CSound"
-sudo make install >> $EDGYR_LOGS/audio.log 2>&1
+sudo make install >> $EDGYR_LOGS/synths.log 2>&1
 sudo /sbin/ldconfig --verbose \
-  >> $EDGYR_LOGS/audio.log 2>&1
+  >> $EDGYR_LOGS/synths.log 2>&1
 cd ..
 rm -fr cs6make
 
@@ -53,11 +55,29 @@ cd chuck-$CHUCK_VERSION/src
 # the Jetson "native" sound infrastructure is ALSA
 echo "Compiling ChucK"
 /usr/bin/time make linux-alsa \
-  >> $EDGYR_LOGS/audio.log 2>&1
+  >> $EDGYR_LOGS/synths.log 2>&1
 echo "Installing ChucK"
 sudo make install \
-  >> $EDGYR_LOGS/audio.log 2>&1
+  >> $EDGYR_LOGS/synths.log 2>&1
 
 sudo rm -fr /usr/local/share/chuck
 sudo mkdir --parents /usr/local/share/chuck
 sudo mv ../examples /usr/local/share/chuck/examples
+
+cd $HOME/src
+rm -fr faust*
+echo "Downloading faust $FAUST_VERSION source"
+curl -Ls https://github.com/grame-cncm/faust/releases/download/$FAUST_VERSION/faust-$FAUST_VERSION.tar.gz \
+  | tar xzf -
+cd faust-$FAUST_VERSION
+
+echo "Symlinking llvm-10-config"
+sudo ln -s /usr/lib/llvm-10/bin/llvm-config /usr/bin/lvm-config
+echo "Compiling faust"
+/usr/bin/time make --jobs=`nproc` world \
+  >> $EDGYR_LOGS/synths.log 2>&1
+echo "Installing faust"
+sudo make install \
+  >> $EDGYR_LOGS/synths.log 2>&1
+echo "Removing llvm-10 symlink"
+sudo rm /usr/bin/llvm-config

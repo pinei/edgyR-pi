7abfa7994bfcdbcfad7859eb17bdab724bd79a86
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-02-18 21:00:33 +0000

    add FluidSynth

--- build-scripts/edgyr/Installers/synths.sh
+++ build-scripts/edgyr/Installers/synths.sh
@@ -5,16 +5,75 @@ set -e
 export CHUCK_VERSION=1.4.0.1
 export CSOUND_VERSION=6.15.0
 export FAUST_VERSION=2.30.5
+export FLUIDSYNTH_VERSION=2.1.7
 echo "Installing build dependencies"
 sudo apt-get install -qqy --no-install-recommends \
   bison \
   flex \
+  fluid-soundfont-gm \
+  fluid-soundfont-gs \
   gettext \
+  ladspa-sdk \
+  libasound2-dev \
+  libdbus-1-dev \
+  libeigen3-dev \
+  libgmm++-dev \
+  libjack-jackd2-dev \
+  liblash-compat-dev \
   libmicrohttpd-dev \
+  libmp3lame-dev \
+  libncurses5-dev \
+  libncursesw5-dev \
+  libpulse-dev \
+  libreadline-dev \
+  libsdl2-dev \
+  libsndfile1-dev \
+  libstk0-dev \
+  libwildmidi-dev \
+  llvm-10 \
+  llvm-10-dev \
+  llvm-10-runtime \
+  llvm-10-tools \
   swig3.0 \
   >> $EDGYR_LOGS/synths.log 2>&1
-
 mkdir --parents $HOME/src
+
+cd $HOME/src
+rm -fr fluidsynth*
+echo "Downloading FluidSynth $FLUIDSYNTH_VERSION source"
+curl -Ls \
+  https://github.com/FluidSynth/fluidsynth/archive/v$FLUIDSYNTH_VERSION.tar.gz \
+  | tar xzf -
+cd fluidsynth-$FLUIDSYNTH_VERSION
+
+echo "Compiling FluidSynth"
+mkdir --parents build; cd build
+cmake -DLIB_SUFFIX="" .. \
+  >> $EDGYR_LOGS/synths.log 2>&1
+/usr/bin/time make --jobs=`nproc` \
+  >> $EDGYR_LOGS/synths.log 2>&1
+echo "Installing FluidSynth"
+sudo make install \
+  >> $EDGYR_LOGS/synths.log 2>&1
+
+cd $HOME/src
+rm -fr faust*
+echo "Downloading faust $FAUST_VERSION source"
+curl -Ls https://github.com/grame-cncm/faust/releases/download/$FAUST_VERSION/faust-$FAUST_VERSION.tar.gz \
+  | tar xzf -
+cd faust-$FAUST_VERSION
+
+echo "Symlinking llvm-10-config"
+sudo ln -s /usr/lib/llvm-10/bin/llvm-config /usr/bin/llvm-config
+echo "Compiling faust"
+/usr/bin/time make --jobs=`nproc` world \
+  >> $EDGYR_LOGS/synths.log 2>&1
+echo "Installing faust"
+sudo make install \
+  >> $EDGYR_LOGS/synths.log 2>&1
+echo "Removing llvm-10 symlink"
+sudo rm /usr/bin/llvm-config
+
 cd $HOME/src
 echo "Downloading CSound $CSOUND_VERSION source"
 rm -fr csound*
@@ -25,13 +84,13 @@ mkdir cs6make
 cd cs6make/
 export CPATH=/usr/include/lame:/usr/include/pulse:$CPATH
 
+echo "Compiling CSound"
 cmake \
   -DBUILD_CUDA_OPCODES=ON \
   -DBUILD_STATIC_LIBRARY=ON \
   -DLAME_HEADER="/usr/include/lame/lame.h" \
   -DPULSEAUDIO_HEADER="/usr/include/pulse/simple.h" \
   ..  >> $EDGYR_LOGS/synths.log 2>&1
-echo "Compiling CSound"
 /usr/bin/time make --jobs=`nproc` \
   >> $EDGYR_LOGS/synths.log 2>&1
 echo "Installing CSound"
@@ -53,7 +112,7 @@ curl -Ls https://chuck.cs.princeton.edu/release/files/chuck-$CHUCK_VERSION.tgz \
 cd chuck-$CHUCK_VERSION/src
 
 # the Jetson "native" sound infrastructure is ALSA
-echo "Compiling ChucK"
+echo "Compiling ChucK for ALSA"
 /usr/bin/time make linux-alsa \
   >> $EDGYR_LOGS/synths.log 2>&1
 echo "Installing ChucK"
@@ -63,21 +122,3 @@ sudo make install \
 sudo rm -fr /usr/local/share/chuck
 sudo mkdir --parents /usr/local/share/chuck
 sudo mv ../examples /usr/local/share/chuck/examples
-
-cd $HOME/src
-rm -fr faust*
-echo "Downloading faust $FAUST_VERSION source"
-curl -Ls https://github.com/grame-cncm/faust/releases/download/$FAUST_VERSION/faust-$FAUST_VERSION.tar.gz \
-  | tar xzf -
-cd faust-$FAUST_VERSION
-
-echo "Symlinking llvm-10-config"
-sudo ln -s /usr/lib/llvm-10/bin/llvm-config /usr/bin/llvm-config
-echo "Compiling faust"
-/usr/bin/time make --jobs=`nproc` world \
-  >> $EDGYR_LOGS/synths.log 2>&1
-echo "Installing faust"
-sudo make install \
-  >> $EDGYR_LOGS/synths.log 2>&1
-echo "Removing llvm-10 symlink"
-sudo rm /usr/bin/llvm-config

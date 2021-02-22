97d551e4025e237085defa9b016963b9aea03169
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-02-21 19:49:47 -0800

    JupyterLab on ML image uses older syntax

--- build-scripts/edgyr/start-jupyter-lab.sh
+++ build-scripts/edgyr/start-jupyter-lab.sh
@@ -7,6 +7,6 @@ echo ""
 echo "Generating Jupyter configuration file"
 jupyter notebook --generate-config
 echo "Enter the same strong password twice"
-jupyter server password
+jupyter notebook password
 echo "If running remotely, browse to port 8888 on this Jetson host instead of 'localhost'"
 SHELL=/bin/bash jupyter lab --no-browser --ip=0.0.0.0

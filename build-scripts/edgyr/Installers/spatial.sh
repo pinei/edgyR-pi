abaeff3ea65ec1dc39206d7b405165d389e29453
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-02-23 15:11:32 -0800

    timing acquired for Nano

--- build-scripts/edgyr/Installers/spatial.sh
+++ build-scripts/edgyr/Installers/spatial.sh
@@ -9,6 +9,7 @@ sudo apt-get install -qqy --no-install-recommends \
   >> $EDGYR_LOGS/spatial.log
 echo "Installing R packages"
 echo "This takes about 17 minutes on an AGX Xavier"
+echo "and 35 minutes on a 4 GB Nano"
 /usr/bin/time Rscript -e "source('~/Installers/R/spatial.R')" \
   >> $EDGYR_LOGS/spatial.log
 gzip -9 $EDGYR_LOGS/spatial.log

87004a2cc948e43433299e7bfcada44a05203638
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-02-23 10:25:45 -0800

    need a bareword name

--- build-scripts/edgyr/Installers/R/rethinking.R
+++ build-scripts/edgyr/Installers/R/rethinking.R
@@ -1,7 +1,17 @@
 #! /usr/bin/env Rscript
 
 source("~/.Rprofile")
-Sys.setenv(MAKE = paste0("make --jobs=", parallel::detectCores()))
+
+# 'rstan' can generate large C code files, which it hands off
+# to 'gcc'. The resulting 'gcc' jobs can swamp RAM on the 
+# Nano. So we limit ourselves to one 'make' job if we have
+# less than 7,000,000 kilobytes of RAM.
+if (as.numeric(Sys.getenv("SYSTEM_RAM_KILOBYTES")) < 7000000) {
+  Sys.setenv(MAKE = paste0("make --jobs=1"))
+} else { 
+  Sys.setenv(MAKE = paste0("make --jobs=", parallel::detectCores()))
+}
+
 install.packages(c(
   "coda",
   "dagitty"

95eda517570b99928682e83088945e36ee512324
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-02-22 16:03:22 -0800

    more unused files

--- build-scripts/edgyr/test-cuda.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-set -e
-
-pushd pocl*/build
-echo "running CUDA tests"
-../tools/scripts/run_cuda_tests
-echo "CUDA tests finished"
-popd
--- build-scripts/edgyr/youtube-dl.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-set -e
-
-# https://ytdl-org.github.io/youtube-dl/download.html
-# https://github.blog/2020-11-16-standing-up-for-developers-youtube-dl-is-back/
-echo "Installing 'youtube-dl' from upstream binary"
-wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
-chmod +x /usr/local/bin/youtube-dl

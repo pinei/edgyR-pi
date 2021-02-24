a037436f1eb52645a3c562292cff40f7558b2560
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-02-23 23:49:55 -0800

    GHC build working

--- build-scripts/internal-pandoc/build-deps.sh
+++ build-scripts/internal-pandoc/build-deps.sh
@@ -4,9 +4,18 @@ echo "Installing build dependencies"
 apt-get update
 apt-get upgrade -y
 apt-get install -qqy --no-install-recommends \
+  build-essential \
+  ca-certificates \
   cabal-install \
+  curl \
   libgmp-dev \
   libnuma-dev \
+  llvm-3.9 \
+  llvm-3.9-dev \
+  llvm-3.9-runtime \
+  llvm-3.9-tools \
   time \
+  wget \
+  xz-utils \
   zlib1g-dev
 apt-get clean
---
+++ build-scripts/internal-pandoc/ghc-8.2.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+set -e
+
+if [ `ram_kilobytes.sh` -lt 7000000 ]
+then
+  export JOBS=1
+else
+  export JOBS=`nproc`
+fi
+echo "JOBS = $JOBS"
+
+cd $SOURCE_DIR
+curl -Ls \
+  https://downloads.haskell.org/~ghc/8.2.2/ghc-8.2.2-src.tar.xz \
+  | tar xJf -
+cd ghc-8.2.2
+./configure
+make --jobs=$JOBS
+make install

8dfc8eeca8176702ec8ec450e3bd5f07102f6d50
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-02-25 13:47:37 -0800

    troubleshooting tools

---
+++ build-scripts/host-setup.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -e
+
+sudo apt-get install -qqy --no-install-recommends \
+  apt-file \
+  file \
+  mlocate \
+  sysstat \
+  time \
+  tree \
+  vim-nox
+echo "Updating 'mlocate' database"
+sudo updatedb
+echo "Updating 'apt-file search' database"
+sudo apt-file update

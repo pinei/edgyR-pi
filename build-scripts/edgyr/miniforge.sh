c242b2b133cc4bf33fe7f135826d9118a0eee903
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-02-27 13:05:34 -0800

    use Miniforge3 - it's smaller

--- build-scripts/edgyr/miniforge.sh
+++ build-scripts/edgyr/miniforge.sh
@@ -6,13 +6,13 @@ mkdir --parents $HOME/Downloads/Installers
 cd $HOME/Downloads/Installers
 echo "Downloading Miniforge-pypy3 installer"
 wget -q \
-  https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge-pypy3-Linux-aarch64.sh
-chmod +x Miniforge-pypy3-Linux-aarch64.sh
+  https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-aarch64.sh
+chmod +x Miniforge3-Linux-aarch64.sh
 
 echo "Installing a fresh copy to '$HOME/miniconda3' ..."
 rm -fr $HOME/mambaforge* $HOME/miniforge* $HOME/miniconda*
-./Miniforge-pypy3-Linux-aarch64.sh -b -p $HOME/miniconda3 \
-  && rm ./Miniforge-pypy3-Linux-aarch64.sh
+./Miniforge3-Linux-aarch64.sh -b -p $HOME/miniconda3 \
+  && rm ./Miniforge3-Linux-aarch64.sh
 
 echo "Initializing conda for 'bash'"
 source $HOME/miniconda3/etc/profile.d/conda.sh
@@ -21,7 +21,6 @@ conda config --set auto_activate_base false
 
 echo "Creating fresh 'r-reticulate' environment"
 /usr/bin/time conda create --name r-reticulate --quiet --force --yes \
-  python=3.6 \
   numpy
 
 echo "Cleaning up"

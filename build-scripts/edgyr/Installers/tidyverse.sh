5cdb2e01ab484d9fc7e7014c441a0d7654faa5e2
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-02-27 13:50:23 -0800

    timing for tidyverse on AGX Xavier

--- build-scripts/edgyr/Installers/tidyverse.sh
+++ build-scripts/edgyr/Installers/tidyverse.sh
@@ -9,7 +9,7 @@ sudo apt-get install -qqy --no-install-recommends \
   >> $EDGYR_LOGS/tidyverse.log
 echo "Installing R packages"
 echo "This takes about 5 minutes on a 4 GB Nano"
-echo "and 3 minutes on an AGX Xavier"
+echo "and 11 minutes on an AGX Xavier"
 /usr/bin/time Rscript -e "source('~/Installers/R/tidyverse.R')" \
   >> $EDGYR_LOGS/tidyverse.log
 gzip -9 $EDGYR_LOGS/tidyverse.log

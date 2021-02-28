8383de1a03e5fcbe97d86c09c820c887c7cb5737
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-02-27 20:17:54 -0800

    commit to local image after stop

--- stop.sh
+++ stop.sh
@@ -2,3 +2,6 @@
 
 echo "Stopping container 'edgyr'"
 sudo docker stop edgyr
+echo "Committing 'edgyr' container to 'edgyr/edgyr:local'"
+sudo docker commit edgyr edgyr/edgyr:local
+sudo docker images

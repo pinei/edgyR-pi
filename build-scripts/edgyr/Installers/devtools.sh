70e4ea40eafe821201d9f97a130fc68fbb307c50
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-02-27 17:08:08 -0800

    revised AGX timing

--- build-scripts/edgyr/Installers/devtools.sh
+++ build-scripts/edgyr/Installers/devtools.sh
@@ -15,7 +15,7 @@ sudo apt-get install -qqy --no-install-recommends \
   >> $EDGYR_LOGS/devtools.log
 echo "Installing R packages"
 echo "This takes about 10 minutes on a 4 GB Nano"
-echo "and 3 minutes on an AGX Xavier"
+echo "and 5.5 minutes on an AGX Xavier"
 /usr/bin/time Rscript -e "source('~/Installers/R/devtools.R')" \
   >> $EDGYR_LOGS/devtools.log
 gzip -9 $EDGYR_LOGS/devtools.log

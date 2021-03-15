a0920f29c12e89d7379960d96143b202cf31e891
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-03-14 20:24:19 -0700

    Julia version bump

--- build-scripts/edgyr/Installers/julia.sh
+++ build-scripts/edgyr/Installers/julia.sh
@@ -4,7 +4,7 @@ set -e
 
 export JULIA_VERSION_MAJOR=1
 export JULIA_VERSION_MINOR=5
-export JULIA_VERSION_PATCH=3
+export JULIA_VERSION_PATCH=4
 echo ""
 echo "Installing 'julia' in '/usr/local'"
 export WHERE="https://julialang-s3.julialang.org/bin/linux/aarch64"

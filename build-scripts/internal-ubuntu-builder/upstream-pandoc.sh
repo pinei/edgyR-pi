1eff3b345a8b7f2d53945438d54c76350a1b2139
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-04-02 12:37:38 -0700

    Pandoc, R and Golang version bumps

--- build-scripts/internal-ubuntu-builder/upstream-pandoc.sh
+++ build-scripts/internal-ubuntu-builder/upstream-pandoc.sh
@@ -6,11 +6,13 @@ apt-get update
 apt-get upgrade -y
 apt-get install -qqy --no-install-recommends \
   ca-certificates \
-  curl
+  curl \
+  vim \
+  wget
 
 export BASE_URL=https://github.com/jgm/pandoc/releases/download/$PANDOC_VERSION
 export FILE=pandoc-$PANDOC_VERSION-linux-arm64.tar.gz
 echo $BASE_URL/$FILE
 curl -Ls $BASE_URL/$FILE \
-  | tar --directory=/usr/local --strip-components=2 --extract --gunzip --file=-
+  | tar --directory=/usr/local --strip-components=1 --extract --gunzip --file=-
 /usr/local/bin/pandoc --version

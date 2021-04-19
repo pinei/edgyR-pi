899ae115ca5e6c49a722142ffdcb4f6716e16172
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-04-18 20:48:15 -0700

    add before and after /usr/local logs

--- build-scripts/edgyr/Installers/R/devtools.R
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env Rscript
-
-source("~/.Rprofile")
-Sys.setenv(MAKE = paste0("make --jobs=", parallel::detectCores()))
-install.packages(c(
-  "devtools",
-  "pkgdown"
-), quiet = TRUE)
-warnings()

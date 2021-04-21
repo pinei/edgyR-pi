a03693e247a44ce04892c7eab1b4dbb6e331ecb2
M. Edward (Ed) Borasky <znmeb@algocompsynth.com>
2021-04-20 20:04:29 -0700

    remove pandoc from global namespace - causes problems later

--- build-scripts/internal-ubuntu-builder/upstream-pandoc.sh
+++ build-scripts/internal-ubuntu-builder/upstream-pandoc.sh
@@ -20,5 +20,5 @@ fi
 export BASE_URL=https://github.com/jgm/pandoc/releases/download/$PANDOC_VERSION
 echo $BASE_URL/$FILE
 curl -Ls $BASE_URL/$FILE \
-  | tar --directory=/usr/local --strip-components=1 --extract --gunzip --file=-
-/usr/local/bin/pandoc --version
+  | tar --directory=/opt --extract --gunzip --file=-
+/opt/pandoc-$PANDOC_VERSION/bin/pandoc --version

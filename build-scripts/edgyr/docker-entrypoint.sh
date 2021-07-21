b9f7f5542a7b5a27946153a9400fc3efb797d900
M. Edward (Ed) Borasky <znmeb@algocompsynth.com>
2021-07-20 22:26:48 -0700

    copy all the binaries!!

--- build-scripts/edgyr/docker-entrypoint.sh
+++ build-scripts/edgyr/docker-entrypoint.sh
@@ -12,4 +12,4 @@ echo "Resetting 'edgyr' password"
 echo "edgyr:${EDGYR_PASSWORD}" | chpasswd
 export RSTUDIO_PORT=`grep www-port /etc/rstudio/rserver.conf | sed 's/^www-port=//'`
 echo "Starting RStudio Server - browse to http://$HOSTNAME:$RSTUDIO_PORT"
-/usr/local/lib/rstudio-server/bin/rserver --server-daemonize 0
+/usr/local/bin/rserver --server-daemonize 0
